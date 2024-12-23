1d736245d104d5c4ed57d320deaea632ae597643
PedroH2003 <pedrohenriquecandidosilva14@gmail.com>
2024-12-23 14:11:51 -0300

    Primeiro endpoint de lista

---
+++ src/main/java/candido/pedro/parking/controller/mapper/ParkingMapper.java
@@ -0,0 +1,24 @@
+package candido.pedro.parking.controller.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import candido.pedro.parking.controller.dto.ParkingDTO;
+import candido.pedro.parking.model.Parking;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO toParkingDTO(Parking parking){
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+    
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList){
+        return parkingList.stream().map(this::toParkingDTO).collect(Collectors.toList());
+    }
+}
